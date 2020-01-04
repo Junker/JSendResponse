0ba93b69f14ef389239629386383745bd1fd524a
Artyum Petrov <artyum@protonmail.com>
2020-01-04 17:22:53 +0100

    Added missing class comment

--- src/JsendResponse/Exceptions/JSendSpecificationViolation.php
+++ src/JsendResponse/Exceptions/JSendSpecificationViolation.php
@@ -4,4 +4,11 @@ namespace Junker\JsendResponse\Exceptions;
 
 use Exception;
 
+/**
+ * Class JSendSpecificationViolation
+ *
+ * Thrown when the response the user is trying to build does not comply with the JSend specification.
+ *
+ * @package Junker\JsendResponse\Exceptions
+ */
 class JSendSpecificationViolation extends Exception {}
